7467c97f14dd3da87a67ff33ee976606941c9910
Kofi B <kofi.boateng@verizon.com>
2024-09-23 18:09:01 -0400

    completed extension(bands/tests)

--- index.test.js
+++ index.test.js
@@ -12,11 +12,45 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
+    test('Making sure musicians page loads', async () => {
+        const response = await request(app).get('/musicians')
+        expect(response.statusCode).toBe(200)
+    })
 
+    test('Making sure page 1  loads', async () => {
+        const response = await request(app).get('/musicians/1')
+        expect(response.statusCode).toBe(200)
+    })
 
+    test('Making sure page 2 loads', async () => {
+        const response = await request(app).get('/musicians/2')
+        expect(response.statusCode).toBe(200)
+    })
 
+    test('Making sure page 3 loads', async () => {
+        const response = await request(app).get('/musicians/3')
+        expect(response.statusCode).toBe(200)
+    })
+})
 
-    
+describe('./bands endpoint', () => {
+
+    test('Making sure bands loads', async () => {
+        const response = await request(app).get('/bands')
+        expect(response.statusCode).toBe(200)
+    })
+    test('Making sure page 1  loads', async () => {
+        const response = await request(app).get('/bands/1')
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('Making sure page 2 loads', async () => {
+        const response = await request(app).get('/bands/2')
+        expect(response.statusCode).toBe(200)
+    })
+
+    test('Making sure page 3 loads', async () => {
+        const response = await request(app).get('/bands/3')
+        expect(response.statusCode).toBe(200)
+    })
 })
--- src/app.js
+++ src/app.js
@@ -1,19 +1,83 @@
 const express = require("express");
 const app = express();
 const { Musician } = require("../models/index")
+const { Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 app.get('/musicians', async (req,res) => {
+    try{
     const music = await Musician.findAll({})
     res.json(music)  
+    } catch (err){
+        res.status(500).send(err.message);
+    }
 })
 
+app.get('/musicians/1', async (req,res) => {
+    try{
+    const music = await Musician.findByPk(1)
+    res.json(music)
+    } catch(err) {
+        res.status(500).send(err.message);
+    }
+})
 
+app.get('/musicians/2', async (req,res) => {
+    try{
+    const music = await Musician.findByPk(2)
+    res.json(music)
+    } catch(err) {
+        res.status(500).send(err.message);
+    }
+})
 
+app.get('/musicians/3', async (req,res) => {
+    try{
+    const music = await Musician.findByPk(3)
+    res.json(music)
+    } catch(err) {
+        res.status(500).send(err.message);
+    }
+})
 
+app.get('/bands', async (req,res) => {
+    try{
+        const band = await Band.findAll()
+        res.json(band)
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+})
+
+app.get('/bands/1', async (req,res) => {
+    try{
+        const band = await Band.findByPk(1)
+        res.json(band)
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+})
+
+app.get('/bands/2', async (req,res) => {
+    try{
+        const band = await Band.findByPk(2)
+        res.json(band)
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+})
+
+app.get('/bands/3', async (req,res) => {
+    try{
+        const band = await Band.findByPk(3)
+        res.json(band)
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+})
 
 
 module.exports = app;
